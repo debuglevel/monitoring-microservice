c85b0bda69083764aeca34c9d008a97df35a1367
Marc Kohaupt <marc.kohaupt@avm-d.de>
2018-10-05 15:54:55 +0200

    Convert Java file to Kotlin

---
+++ src/main/kotlin/de/debuglevel/monitoring/TrustModifier.kt
@@ -0,0 +1,59 @@
+package de.debuglevel.monitoring
+
+import javax.net.ssl.*
+import java.net.HttpURLConnection
+import java.security.KeyManagementException
+import java.security.KeyStoreException
+import java.security.NoSuchAlgorithmException
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+
+object TrustModifier {
+    private val TRUSTING_HOSTNAME_VERIFIER = TrustingHostnameVerifier()
+    private var factory: SSLSocketFactory? = null
+
+    /** Call this with any HttpURLConnection, and it will
+     * modify the trust settings if it is an HTTPS connection.  */
+    @Throws(KeyManagementException::class, NoSuchAlgorithmException::class, KeyStoreException::class)
+    fun relaxHostChecking(conn: HttpURLConnection) {
+
+        if (conn is HttpsURLConnection) {
+            val factory = prepFactory(conn)
+            conn.sslSocketFactory = factory!!
+            conn.hostnameVerifier = TRUSTING_HOSTNAME_VERIFIER
+        }
+    }
+
+    @Synchronized
+    @Throws(NoSuchAlgorithmException::class, KeyStoreException::class, KeyManagementException::class)
+    internal fun prepFactory(httpsConnection: HttpsURLConnection): SSLSocketFactory? {
+
+        if (factory == null) {
+            val ctx = SSLContext.getInstance("TLS")
+            ctx.init(null, arrayOf<TrustManager>(AlwaysTrustManager()), null)
+            factory = ctx.socketFactory
+        }
+        return factory
+    }
+
+    private class TrustingHostnameVerifier : HostnameVerifier {
+        override fun verify(hostname: String, session: SSLSession): Boolean {
+            return true
+        }
+    }
+
+    private class AlwaysTrustManager : X509TrustManager {
+        @Throws(CertificateException::class)
+        override fun checkClientTrusted(arg0: Array<X509Certificate>, arg1: String) {
+        }
+
+        @Throws(CertificateException::class)
+        override fun checkServerTrusted(arg0: Array<X509Certificate>, arg1: String) {
+        }
+
+        override fun getAcceptedIssuers(): Array<X509Certificate>? {
+            return null
+        }
+    }
+
+}
