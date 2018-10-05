c664099d2faa6e0341906cdb25bd209ad102db6a
Marc Kohaupt <debuglevel@gmail.com>
2018-10-06 00:22:52 +0200

    Add HTML output

--- src/main/kotlin/de/debuglevel/monitoring/rest/RestEndpoint.kt
+++ src/main/kotlin/de/debuglevel/monitoring/rest/RestEndpoint.kt
@@ -4,6 +4,7 @@ import com.google.gson.GsonBuilder
 import de.debuglevel.microservices.utils.apiversion.apiVersion
 import de.debuglevel.microservices.utils.spark.configuredPort
 import de.debuglevel.microservices.utils.status.status
+import de.debuglevel.monitoring.Monitoring
 import de.debuglevel.monitoring.ServiceState
 import de.debuglevel.monitoring.StateChecker
 import mu.KotlinLogging
@@ -24,11 +25,19 @@ class RestEndpoint {
 
     val stateChecker = StateChecker
 
-    private fun ServiceState?.stateToString(): String {
+    private fun ServiceState?.toPlaintextString(): String {
         return when (this) {
             ServiceState.Up -> "[ UP ]"
             ServiceState.Down -> "[DOWN]"
             else -> "[ ?? ]"
+        }
+    }
+
+    private fun ServiceState?.toHtmlString(): String {
+        return when (this) {
+            ServiceState.Up -> "up"
+            ServiceState.Down -> "down"
+            else -> "unknown"
         }
     }
 
@@ -65,14 +74,20 @@ class RestEndpoint {
                 get("/") {
                     val monitorings = stateChecker.getMonitorings()
 
-                    if (request.contentType() == "text/plain") {
+                    if (request.headers("Accept").contains("text/plain")) {
+                        logger.debug { "Processing GET request for text/plain..." }
                         type(contentType = "text/plain")
                         monitorings
                                 .asSequence()
                                 .sortedBy { it.url }
-                                .map { "${it.serviceState.stateToString()}\t${it.lastSeen?.toLastSeenString()}\t${it.url}" }
+                                .map { "${it.serviceState.toPlaintextString()}\t${it.lastSeen?.toLastSeenString()}\t${it.url}" }
                                 .joinToString("\n")
+                    } else if (request.headers("Accept").contains("text/html")) {
+                        logger.debug { "Processing GET request for text/html..." }
+                        type(contentType = "text/html")
+                        buildHtml(monitorings)
                     } else {
+                        logger.debug { "Processing GET request as application/json..." }
                         type(contentType = "application/json")
                         GsonBuilder()
                                 .setPrettyPrinting()
@@ -112,5 +127,68 @@ class RestEndpoint {
                 }
             }
         }
+    }
+
+    private fun buildHtml(monitorings: Set<Monitoring>): String {
+        var html = ""
+
+        html +=
+                """
+                    <!DOCTYPE html>
+                    <html>
+                      <head>
+                        <meta charset="UTF-8">
+                        <title>Monitorings</title>
+                        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                        <style>
+                        tr.Up { background-color: #C4FEAC; }
+                        tr.Down { background-color: #FEACAC; }
+                        tr.Unknown { background-color: #EAEAEA; }
+                        </style>
+                      </head>
+                      <body>
+                      <table class="table table-hover table-sm">
+                      <thead>
+                      <tr>
+                      <th scope="col" class="state">State</th>
+                      <th scope="col" class="lastSeen">last Seen</th>
+                      <th scope="col" class="url">URL</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      """.trimIndent()
+
+        for (monitoring in monitorings) {
+            val trClass = when (monitoring.serviceState) {
+                ServiceState.Up -> "table-success"
+                ServiceState.Down -> "table-danger"
+                else -> "table-light"
+            }
+
+            html += """
+                <tr class="${monitoring.serviceState} $trClass">
+                """.trimIndent()
+
+            html +=
+                    """
+                        <td class="state ${monitoring.serviceState}">${monitoring.serviceState.toHtmlString()}</td>
+                        <td class="lastSeen">${monitoring.lastSeen.toLastSeenString()}</td>
+                        <td class="url"><a href="${monitoring.url}">${monitoring.url}</a></td>
+                    """.trimIndent()
+
+            """
+                </tr>
+            """.trimIndent()
+        }
+
+        html +=
+                """
+            </tbody>
+            </table>
+                      </body>
+                    </html>
+                """.trimIndent()
+
+        return html
     }
 }
