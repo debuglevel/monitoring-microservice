3aa83754000bb7900f92870738cbf35e68b28a5d
Marc Kohaupt <debuglevel@gmail.com>
2020-08-30 12:37:38 +0200

    Returning more information on GET request

--- src/main/kotlin/de/debuglevel/monitoring/monitoring/GetMonitoringResponse.kt
+++ src/main/kotlin/de/debuglevel/monitoring/monitoring/GetMonitoringResponse.kt
@@ -1,13 +1,26 @@
 package de.debuglevel.monitoring.monitoring
 
+import de.debuglevel.monitoring.ServiceState
+import java.time.LocalDateTime
+
 data class GetMonitoringResponse(
     val id: Int?,
     val url: String,
-    val name: String
+    val ip: String,
+    val name: String,
+    val createdOn: LocalDateTime,
+    val lastCheck: LocalDateTime?,
+    val lastModified: LocalDateTime,
+    val serviceState: ServiceState,
 ) {
     constructor(monitoring: Monitoring) : this(
         id = monitoring.id,
         url = monitoring.url,
-        name = monitoring.name ?: ""
+        ip = monitoring.ip ?: "resolving failed",
+        name = monitoring.name ?: "",
+        createdOn = monitoring.createdOn,
+        lastCheck = monitoring.lastCheck,
+        lastModified = monitoring.lastModified,
+        serviceState = monitoring.serviceState
     )
 }
